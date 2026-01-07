b344812f81fb054a95ae5ed965e849d3d62dbf1c
rajan0917 <132059861+rajan0917@users.noreply.github.com>
2026-01-07 11:59:53 +0530

    Update variables.tf

--- envs/test/variables.tf
+++ envs/test/variables.tf
@@ -1,8 +1,11 @@
 variable "region" {
-  type = string
-  default = "us-east-1"
+  type    = string
+  default = "ap-south-1"
 }
 variable "env" {
   type = string
 }
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
