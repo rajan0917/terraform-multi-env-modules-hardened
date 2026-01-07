24746bb1f9a8da52434615b1414335b05be1ef2c
rajan0917 <132059861+rajan0917@users.noreply.github.com>
2026-01-07 11:43:27 +0530

    Update backend.tf

--- envs/stage/backend.tf
+++ envs/stage/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-yourbucket-stage"
-    key            = "stage/terraform.tfstate"
-    region         = var.region
-    dynamodb_table = "tf-locks-stage"
+    bucket         = "tf-state-mybucket-dev"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile   = false
     encrypt        = true
   }
 }
